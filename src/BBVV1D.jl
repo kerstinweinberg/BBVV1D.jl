769370167da18ecc80228e0a6600bed167acf24d
Kai Partmann <kai.partmann@gmail.com>
2023-09-15 08:51:37 +0200

    Renaming

--- src/BBVV1D.jl
+++ src/BBVV1D.jl
@@ -29,7 +29,6 @@ struct VelocityBC
     point_id_set::Vector{Int}
 end
 
-
 """
 MAIN FUNCTION
 """
@@ -40,7 +39,7 @@ function simulation(pc::PointCloud, mat::BondBasedMaterial, bcs::Vector{Velocity
         print("initialization...")
 
         # simulation parameters
-        bonds, init_dists, hood_range = find_bonds(pc, mat.δ)
+        neighbor, initial_distance, bond_ids_of_point = find_bonds(pc, mat.δ)
         cells = get_cells(pc.n_points)
 
         position = copy(pc.position)
@@ -51,20 +50,18 @@ function simulation(pc::PointCloud, mat::BondBasedMaterial, bcs::Vector{Velocity
         b_int = zeros(pc.n_points)
 
         println("\r✔ initialization   ")
+        print("time loop...")
 
-        Δt = calc_stable_timestep(pc, mat, bonds, hood_range, init_dists)
-        Δt½ = 0.5Δt
+        Δt = calc_stable_timestep(pc, mat, neighbor, bond_ids_of_point, initial_distance)
 
         export_vtk(position, displacement, cells, export_path, 0, 0)
-
-        print("time loop...")
 
         for timestep in 1:n_timesteps
             time = timestep * Δt
 
             # update of velocity_half
             for i in 1:pc.n_points
-                velocity_half[i] = velocity[i] + acceleration[i] * Δt½
+                velocity_half[i] = velocity[i] + acceleration[i] * 0.5Δt
             end
 
             # apply the boundary conditions
@@ -84,21 +81,20 @@ function simulation(pc::PointCloud, mat::BondBasedMaterial, bcs::Vector{Velocity
             # compute the internal force density b_int
             b_int .= 0
             for i in 1:pc.n_points
-                for cbond in hood_range[i]
-                    j = bonds[cbond]
-                    L = init_dists[cbond]
+                for current_bond in bond_ids_of_point[i]
+                    j = neighbor[current_bond]
+                    L = initial_distance[current_bond]
                     Δxij = position[j] - position[i]
                     l = abs(Δxij)
                     ε = (l - L) / L
-                    temp = mat.bc * ε / l * pc.volume[j]
-                    b_int[i] += temp * Δxij
+                    b_int[i] += mat.bc * ε / l * pc.volume[j] * Δxij
                 end
             end
 
             # solve the equation of motion
             for i in 1:pc.n_points
                 acceleration[i] = b_int[i] / mat.rho
-                velocity[i] = velocity_half[i] + acceleration[i] * Δt½
+                velocity[i] = velocity_half[i] + acceleration[i] * 0.5Δt
             end
 
             if mod(timestep, export_freq) == 0
@@ -112,7 +108,7 @@ function simulation(pc::PointCloud, mat::BondBasedMaterial, bcs::Vector{Velocity
     open(joinpath(export_path, "logfile.log"), "w+") do io
         write(io, "simulation completed after $walltime seconds (wall time)\n\n")
         write(io, "number of points = $(pc.n_points)\n")
-        write(io, "number of bonds = $(length(bonds))\n")
+        write(io, "number of bonds = $(length(neighbor))\n")
         write(io, "Δt = $Δt\n")
     end
 
@@ -120,35 +116,35 @@ function simulation(pc::PointCloud, mat::BondBasedMaterial, bcs::Vector{Velocity
 end
 
 function find_bonds(pc::PointCloud, δ::Float64)
-    bonds = Vector{Int}()
-    init_dists = Vector{Float64}()
-    hood_range = fill(0:0, pc.n_points)
-    n_bonds = 0
+    neighbor = Vector{Int}()
+    initial_distance = Vector{Float64}()
+    bond_ids_of_point = fill(0:0, pc.n_points)
+    bond_counter = 0
     for i in 1:pc.n_points
-        hood_range_start = n_bonds + 1
+        bond_ids_of_point_start = bond_counter + 1
         for j in 1:pc.n_points
             if i !== j
                 L = abs(pc.position[j] - pc.position[i])
                 if L <= δ
-                    n_bonds += 1
-                    push!(bonds, j)
-                    push!(init_dists, L)
+                    bond_counter += 1
+                    push!(neighbor, j)
+                    push!(initial_distance, L)
                 end
             end
         end
-        hood_range[i] = hood_range_start:n_bonds
+        bond_ids_of_point[i] = bond_ids_of_point_start:bond_counter
     end
-    @assert length(bonds) == n_bonds
-    return bonds, init_dists, hood_range
+    @assert length(neighbor) == bond_counter
+    return neighbor, initial_distance, bond_ids_of_point
 end
 
-function calc_stable_timestep(pc, mat, bonds, hood_range, init_dists)
+function calc_stable_timestep(pc, mat, neighbor, bond_ids_of_point, initial_distance)
     timesteps = fill(typemax(Float64), pc.n_points)
     for i in 1:pc.n_points
         dtsum = 0.0
-        for cbond in hood_range[i]
-            j = bonds[cbond]
-            L = init_dists[cbond]
+        for current_bond in bond_ids_of_point[i]
+            j = neighbor[current_bond]
+            L = initial_distance[current_bond]
             dtsum += pc.volume[j] * mat.bc / L
         end
         timesteps[i] = sqrt(2 * mat.rho / dtsum)
